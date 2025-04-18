de36584bb1d236357244df99ad5dedbf4039986c
IVÁN GARCÍA GRIS <ivangris2709@gmail.com>
2025-04-18 21:47:36 +0200

    feat: crear componentes de navegación y pie de página con sus estilos

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Github, Linkedin, X, Mail } from "iconoir-react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    let date = new Date();
+    let year = date.getFullYear();
+
+    return (
+        <footer className="bg-[rgba(255,255,255,0.1)] font-bold p-2 px-4 md:px-10 lg:px-10 font-[Raleway]">
+            <div className="w-full h-auto flex flex-col md:flex-row justify-between items-center py-4 px-4">
+                <div className="text-white w-full md:w-1/2 flex flex-col md:flex-row justify-center md:justify-start items-center text-center md:text-left gap-2 mb-4 md:mb-0">
+                    <div>
+                        <h3 className="text-sm md:text-base">
+                            Diseñado y desarrollado por Iván García
+                        </h3>
+                    </div>
+                    <div className="hidden md:block mx-2">•</div>
+                    <div>
+                        <h3 className="text-sm md:text-base">
+                            Copyright &copy; {year} IG.
+                        </h3>
+                    </div>
+                </div>
+
+                <div className="flex justify-center items-center gap-5 text-base md:text-xl">
+                    <a href="https://github.com/username">
+                        <Github className="text-white hover:text-[#6e5494]" />
+                    </a>
+                    <a href="https://linkedin.com/in/username">
+                        <Linkedin className="text-white hover:text-[#0a66c2]" />
+                    </a>
+                    <a href="https://twitter.com/username">
+                        <X className="text-white hover:opacity-60" />
+                    </a>
+                    <a
+                        href="mailto:email@email.com"
+                        aria-label="Gmail"
+                        className="text-white hover:text-[#ea4335]"
+                    >
+                        <Mail />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { Home, User, Presentation, JournalPage } from "iconoir-react";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <header className="relative p-2 px-4 md:px-8 lg:px-8 w-full h-20 font-[Raleway]">
+            <div className="absolute inset-0 bg-gradient-to-b from-[hsla(0,0%,100%,0)] to-[rgba(107,114,128,0.2)]"></div>
+
+            <nav className="relative z-10 w-full h-auto flex justify-between items-center p-2">
+                {/* Logo */}
+                <div className="flex-shrink-0">
+                    <h2 className="text-white text-xl md:text-3xl font-bold">
+                        IG
+                    </h2>
+                </div>
+
+                {/* Navegación principal */}
+                <ul className="text-base md:text-xl flex justify-center items-center gap-6 md:gap-8 lg:gap-12 mx-4 font-[Raleway]">
+                    <li className="text-white hover:text-gray-200 transition-colors">
+                        <a href="/" className="flex items-center gap-2">
+                            <Home
+                                className="w-5 h-5 md:w-6 md:h-6"
+                                strokeWidth="2.5"
+                            />
+                            <span>Inicio</span>
+                        </a>
+                    </li>
+                    <li className="text-white hover:text-gray-200 transition-colors">
+                        <a href="/about" className="flex items-center gap-2">
+                            <User
+                                className="w-5 h-5 md:w-6 md:h-6"
+                                strokeWidth="2.5"
+                            />
+                            <span>Sobre mi</span>
+                        </a>
+                    </li>
+                    <li className="text-white hover:text-gray-200 transition-colors">
+                        <a href="/projects" className="flex items-center gap-2">
+                            <Presentation
+                                className="w-5 h-5 md:w-6 md:h-6"
+                                strokeWidth="2.5"
+                            />
+                            <span>Proyectos</span>
+                        </a>
+                    </li>
+                    <li className="text-white hover:text-gray-200 transition-colors">
+                        <a
+                            href="/curriculum"
+                            className="flex items-center gap-2"
+                        >
+                            <JournalPage
+                                className="w-5 h-5 md:w-6 md:h-6"
+                                strokeWidth="2"
+                            />
+                            <span>Curriculum</span>
+                        </a>
+                    </li>
+                </ul>
+
+                {/* Botón para cambiar el tema oscuro/claro */}
+                <div className="flex-shrink-0 ml-4"></div>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
